49f6471b2ec688d8c95c99536c7e4b099aa2e94b
nozwock <57829219+nozwock@users.noreply.github.com>
2022-12-04 01:45:09 +0530

    feat: add get username fn for windows

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 mod gui;
 pub mod utils;
+pub mod winutil;
 pub use gui::AMEApp;
---
+++ src/winutil.rs
@@ -0,0 +1,32 @@
+#[cfg(windows)]
+use {
+    std::slice,
+    windows::{core::PWSTR, Win32::System::WindowsProgramming::GetUserNameW},
+};
+
+#[cfg(windows)]
+/// Retrieves the name of the user associated with the current thread.
+pub fn get_username() -> Option<String> {
+    // https://learn.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-getusernamew
+    // https://stackoverflow.com/questions/68716774/
+    let mut cb_buffer = 257_u32;
+
+    // Create a buffer of the required size
+    let mut buffer = Vec::<u16>::with_capacity(cb_buffer as usize);
+    // Construct a `PWSTR` by taking the address to the first element in the buffer
+    let lp_buffer = PWSTR(buffer.as_mut_ptr());
+
+    let result = unsafe { GetUserNameW(lp_buffer, &mut cb_buffer) };
+
+    let mut user_name = None;
+    // If the API returned success, and more than 0 characters were written
+    if result.as_bool() && cb_buffer > 0 {
+        // Construct a slice over the valid data
+        let buffer = unsafe { slice::from_raw_parts(lp_buffer.0, cb_buffer as usize - 1) };
+
+        // And convert from UTF-16 to Rust's native encoding
+        user_name = Some(String::from_utf16_lossy(buffer));
+    }
+
+    user_name
+}
