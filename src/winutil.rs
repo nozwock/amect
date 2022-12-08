1811a175ad126a620d6a4003db0dd14b802ba71c
nozwock <57829219+nozwock@users.noreply.github.com>
2022-12-09 03:18:02 +0530

    feat: add set_profile_img api

--- src/winutil.rs
+++ src/winutil.rs
@@ -1,9 +1,12 @@
-use std::fs;
-
 // #[cfg(windows)]
 use {
     anyhow::{bail, Result},
-    std::{path::Path, process::Command, slice, str},
+    std::{
+        fs,
+        path::Path,
+        process::{Command, Stdio},
+        slice, str,
+    },
     widestring::U16CString,
     windows::{
         core::{PCWSTR, PWSTR},
@@ -175,6 +178,7 @@ pub fn wmic_set_username(curr: &str, new: &str) -> Result<()> {
             "rename",
             new,
         ])
+        .stdout(Stdio::null())
         .status()?;
 
     if !result.success() {
@@ -188,6 +192,7 @@ pub fn wmic_set_username(curr: &str, new: &str) -> Result<()> {
 pub fn net_set_password(username: &str, password: &str) -> Result<()> {
     let result = Command::new("NET")
         .args(["user", username, password])
+        .stdout(Stdio::null())
         .status()?;
 
     if !result.success() {
@@ -206,6 +211,7 @@ pub fn net_set_user_elevated(enable: bool, username: &str) -> Result<()> {
     let action = if enable { "/add" } else { "/delete" };
     let result = Command::new("NET")
         .args(["localgroup", "administrators", username, action])
+        .stdout(Stdio::null())
         .status()?;
 
     if !result.success() {
@@ -243,7 +249,8 @@ pub fn set_lockscreen_blur(enable: bool) -> Result<()> {
     }
 }
 
-pub fn set_lockscreen_img(user_sid: &str, image: impl AsRef<Path>) -> Result<()> {
+// #[cfg(windows)]
+pub fn set_lockscreen_img<P: AsRef<Path>>(user_sid: &str, image_path: P) -> Result<()> {
     // Supposedly necessary for updated 21H2+ versions if RotatingLockScreenEnabled is not already set to 0
     let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
     let (key, _disp) = hklm.create_subkey(format!(
@@ -269,27 +276,31 @@ pub fn set_lockscreen_img(user_sid: &str, image: impl AsRef<Path>) -> Result<()>
     {
         if !Command::new("takeown")
             .args(["/f", img.as_str()])
+            .stdout(Stdio::null())
             .status()?
             .success()
             && !Command::new("icacls")
                 .args([img.as_str(), "/reset"])
+                .stdout(Stdio::null())
                 .status()?
                 .success()
         {
             bail!("failed to take ownership of old images");
         }
         // TODO: maybe do img conversion rather than just renaming the extension
-        fs::copy(image, Path::new(img.as_str()))?;
+        fs::copy(&image_path, Path::new(img.as_str()))?;
     }
 
     // clear cache
     let systemdata = format!("{}\\{}", programdata, r#"Microsoft\Windows\SystemData"#);
     if !Command::new("takeown")
         .args(["/r", "/d", "y", "/f", systemdata.as_str()])
+        .stdout(Stdio::null())
         .status()?
         .success()
         && !Command::new("icacls")
             .args([systemdata.as_str(), "/reset"])
+            .stdout(Stdio::null())
             .status()?
             .success()
     {
@@ -301,13 +312,77 @@ pub fn set_lockscreen_img(user_sid: &str, image: impl AsRef<Path>) -> Result<()>
     todo!()
 }
 
-fn set_profile_img(user_sid: &str, image: impl AsRef<Path>) -> Result<()> {
-    todo!()
+// #[cfg(windows)]
+/// needs testing
+pub fn set_profile_img<P: AsRef<Path>>(user_sid: &str, image_path: P) -> Result<()> {
+    let pfp = image::open(&image_path)?;
+
+    let public = get_env_var("public")?;
+    let usr_pfp_dir = format!("{}\\AccountPictures\\{}", public, user_sid);
+
+    fs::create_dir_all(usr_pfp_dir.as_str())?;
+    if !Command::new("takeown")
+        .args(["/r", "/d", "y", "/f", usr_pfp_dir.as_str()])
+        .stdout(Stdio::null())
+        .status()?
+        .success()
+        && !Command::new("icacls")
+            .args([usr_pfp_dir.as_str(), "/reset", "/t"])
+            .stdout(Stdio::null())
+            .status()?
+            .success()
+    {
+        bail!("failed to take ownership of directory");
+    }
+    for entry in fs::read_dir(usr_pfp_dir.as_str())? {
+        fs::remove_file(entry?.path())?;
+    }
+
+    let sizes = [32, 40, 48, 64, 96, 192, 208, 240, 424, 448, 1080];
+    for size in sizes.iter() {
+        let scaled = pfp.resize_exact(*size, *size, image::imageops::FilterType::Lanczos3);
+        scaled.save(format!("{}\\{}x{}.png", usr_pfp_dir.as_str(), *size, *size))?;
+    }
+
+    let hklm = RegKey::predef(HKEY_LOCAL_MACHINE);
+    let usr_pfp_key = format!(
+        "{}\\{}",
+        r#"SOFTWARE\Microsoft\Windows\CurrentVersion\AccountPicture\Users"#, user_sid
+    );
+    // TODO: figure how to run the reg delete as system user (for compaitibility) ln 502
+    if let Err(err) = hklm.delete_subkey_all(usr_pfp_key.as_str()) {
+        match err.kind() {
+            std::io::ErrorKind::NotFound => { /* ignore */ }
+            _ => Err(err)?,
+        }
+    }
+
+    let (key, _disp) = hklm.create_subkey(usr_pfp_key)?;
+    for size in sizes {
+        key.set_value(
+            format!("Image{}", size),
+            &format!("{}\\{}x{}.png", usr_pfp_dir.as_str(), size, size),
+        );
+    }
+
+    RegKey::predef(HKEY_USERS)
+        .create_subkey(format!(
+            "{}\\{}",
+            user_sid, r#"SOFTWARE\OpenShell\StartMenu\Settings"#
+        ))?
+        .0
+        .set_value(
+            "UserPicturePath",
+            &format!("{}\\{}x{}.png", usr_pfp_dir, 448, 448),
+        );
+
+    Ok(())
 }
 
-/// This is not ideal
+// #[cfg(windows)]
+/// This is not ideal due to the use of `trim()`.
 ///
-/// **NOTE:** Uses `powershell`
+/// Uses `powershell`.
 pub fn get_env_var(env_var: &str) -> Result<String> {
     str::from_utf8(
         Command::new("powershell")
