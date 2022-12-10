f82d7caea971c40370dc9061c7cbc1c3af77233b
nozwock <57829219+nozwock@users.noreply.github.com>
2022-12-11 01:51:08 +0530

    feat: add an all images filter in browse_image_file

--- src/utils.rs
+++ src/utils.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 
 pub fn browse_image_file() -> Option<PathBuf> {
     FileDialog::new()
+        .add_filter("All Images", &["png", "jpg", "jpeg", "bmp"])
         .add_filter("PNG Image", &["png"])
         .add_filter("JPEG Image", &["jpg", "jpeg"])
         .add_filter("Bitmap Image", &["bmp"])
